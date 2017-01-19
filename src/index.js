74120bf218a2149691eb0f7b0b4aa6229b8eefbe
= <bradmedeiros0@gmail.com>
2017-01-18 22:40:35 -0800

    made layout

--- src/index.js
+++ src/index.js
@@ -5,20 +5,12 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Provider } from 'react-redux';
 
-import { container as Appbar } from './components/appbar/Appbar.js';
-import { container as Grid } from './components/Grid/Grid.js';
-import {  container as SelectionOverlay } from './components/SelectionOverlay/SelectionOverlay';
 import createLogger from 'redux-logger';
 import { applyMiddleware, createStore } from 'redux';
 import { fromJS, List } from 'immutable';
 
 import createRoutes from './Routing';
-
-import Menu from './components/menu/menu';
-import Footer from './components/footer/footer';
-import { container as AddGrid } from './components/addgrid/AddGrid';
-import MinimalMenu from './components/menu/minimalMenu';
-import { ActionHome } from 'material-ui/svg-icons';
+import Layout from './layout';
 
 const initialState = fromJS({
   menuExpanded: false,
@@ -70,25 +62,7 @@ const store = createStore(reducer, applyMiddleware(createLogger()));
 const App = () => (
   <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
     <Provider store={store} >
-      <div style={{ width: '100vw', height: '100vh', top: 0 , overflow: 'hidden' }}>
-        <Appbar />
-        <SelectionOverlay />
-        <Grid />
-        <Menu
-          buttonLabels={fromJS(['home','creation','store','disconnect'])}
-          style={
-            {
-              width: 210,
-              height: '94%',
-              position: 'fixed',
-              left: 5,
-              top: 55,
-              zIndex: 500,
-            }
-          }
-        />
-        <Footer />
-      </div>
+      <Layout />
     </Provider>
   </MuiThemeProvider>
 );
---
+++ src/layout.js
@@ -0,0 +1,33 @@
+
+import React, { Component, PropTypes } from 'react';
+import { fromJS } from 'immutable';
+
+import { container as Appbar } from './components/appbar/Appbar.js';
+import { container as Grid } from './components/Grid/Grid.js';
+import {  container as SelectionOverlay } from './components/SelectionOverlay/SelectionOverlay';
+import Menu from './components/menu/menu';
+import Footer from './components/footer/footer';
+
+import { container as AddGrid } from './components/addgrid/AddGrid';
+import MinimalMenu from './components/menu/minimalMenu';
+import { ActionHome } from 'material-ui/svg-icons';
+
+
+class Layout extends Component {
+  render()  {
+    return (
+      <div style={{ width: '100vw', height: '100vh', top: 0 , overflow: 'hidden' }}>
+      <Appbar />
+      <SelectionOverlay />
+      <Grid />
+      <Menu
+        buttonLabels={fromJS(['home','creation','store','disconnect'])}
+        style={{ width: 210, height: '94%', position: 'fixed', left: 5, top: 55, zIndex: 500 }}
+      />
+      <Footer />
+    </div>
+    )
+  }
+}
+
+export default Layout;
