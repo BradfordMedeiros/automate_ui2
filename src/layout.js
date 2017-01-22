c13c8f8da08200604bea309c9af0f3fe906ad586
= <bradmedeiros0@gmail.com>
2017-01-21 17:36:35 -0800

    added desktop/mobile sizing via media query

--- src/layout.js
+++ src/layout.js
@@ -1,10 +1,12 @@
 
 import React, { Component, PropTypes } from 'react';
 import { fromJS, List, Map } from 'immutable';
+import { Desktop, Mobile } from './util/ViewportSizing';
 import { container as Appbar } from './containers/Appbar';
 import { container as Grid } from './containers/Grid';
 import {  container as SelectionOverlay } from './containers/Overlay';
 import Menu from './components/menu/menu';
+import MinimalMenu from './components/menu/minimalMenu';
 import Footer from './components/footer/footer';
 
 const layout = List([
@@ -21,22 +23,39 @@ const tileNameToTile = Map({
   c: <div>tile ---c</div>,
 });
 
-const styles = {
-  app : { width: '100vw', height: '100vh', margin: 0, padding: 0, left: 0, top: 0 , overflow: 'hidden' },
+const appStyle = { width: '100vw', height: '100vh', margin: 0, padding: 0, left: 0, top: 0 , overflow: 'hidden' };
+const desktopStyles = {
   appbar: { height: 50, width: '100%', top: 0, zIndex: 200 },
   grid: { top: 50, bottom: 10, left: 210, right: 0 },
+  menu: { width: 210, height: '94%', position: 'fixed', left: 1, top: 48, zIndex: 500 },
+};
+const mobileStyles = {
+  appbar: { height: 50, width: '100%', top: 0, zIndex: 200 },
+  grid: { top: 50, bottom: 10, left: 55, right: 0 },
   menu: { width: 210, height: '94%', position: 'fixed', left: 1, top: 48, zIndex: 500 },
 };
 
 class Layout extends Component {
   render()  {
     return (
-      <div style={styles.app}>
-        <Appbar style={styles.appbar} />
-        <Menu buttonLabels={fromJS(['home','creation','store','disconnect'])} style={styles.menu} />
-        <Grid layout={layout} tileNames={tileNames} tileNameToTile={tileNameToTile} style={styles.grid} />
-        <SelectionOverlay  />
-        <Footer />
+      <div style={appStyle}>
+        <Desktop>
+          <Menu buttonLabels={fromJS(['home','creation','store','disconnect'])} style={desktopStyles.menu} />
+          <Appbar style={desktopStyles.appbar} />
+          <Grid layout={layout} tileNames={tileNames} tileNameToTile={tileNameToTile} style={desktopStyles.grid} />
+          <SelectionOverlay  />
+          <Footer />
+        </Desktop>
+
+        <Mobile>
+          <MinimalMenu />
+          <Appbar style={mobileStyles.appbar} />
+          <Grid layout={layout} tileNames={tileNames} tileNameToTile={tileNameToTile} style={mobileStyles.grid} />
+          <SelectionOverlay  />
+          <Footer />
+
+        </Mobile>
+
       </div>
     )
   }
