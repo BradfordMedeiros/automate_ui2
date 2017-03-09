930d054c0fd60d722d58a4ccf9cba2cd805e6901
= <bradmedeiros0@gmail.com>
2017-03-09 01:06:41 -0800

    -

---
+++ src/containers/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Menu from '../components/menu/menu';
+import { setContent, expandMenu } from './grid/module';
+
+const MenuContainer = ({ setSSHContent, ...otherProps}) => (
+  <Menu {...otherProps}  buttonLabels={
+    [
+      'home',
+      'creation',
+    {
+      label: 'ssh',
+      onClick: setSSHContent,
+    },
+    'disconnect']} />
+);
+
+const mapStateToProps = (state) => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+  setSSHContent: () => {
+    dispatch(setContent(<div>hello world</div>))
+    dispatch(expandMenu(true));
+  }
+});
+
+export const container = connect(mapStateToProps, mapDispatchToProps)(MenuContainer);
--- src/containers/menu/MinimalMenu.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by brad on 3/8/17.
- */
