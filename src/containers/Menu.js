302e40acf1f9f6318960f29deb2765b3b02b4d43
= <bradmedeiros0@gmail.com>
2017-03-20 02:37:31 -0700

    stubbed router + events

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,31 +1,92 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Menu from '../components/menu/menu';
+import RouterOptions from '../router_options/RouterOptions';
+import EventLog from '../event_log/EventLog';
 import { setContent, expandMenu, setGridIsOpen } from './grid/module';
 
-import SSH from '../ssh/ssh';
-
-const MenuContainer = ({ setSSHContent, ...otherProps}) => (
+const MenuContainer = (
+  {
+    setSSHContent,
+    closeSSHContent,
+    setRouterOptions,
+    closeMenu,
+    menuIsExpanded,
+    addGridExpanded,
+    ...otherProps
+  }) => (
   <Menu {...otherProps}  buttonLabels={
     [
-      'home',
-      'creation',
-    {
-      label: 'ssh',
-      onClick: setSSHContent,
-    },
-    'disconnect']} />
+      {
+        label: 'home',
+        onClick: () => {
+          if (menuIsExpanded){
+            closeMenu();
+          }
+          if (addGridExpanded){
+            closeSSHContent();
+          }
+        }
+      },
+      {
+        label: 'router',
+        onClick: () => {
+          if (menuIsExpanded || addGridExpanded){
+            closeMenu();
+            closeSSHContent();
+          }else{
+            setRouterOptions();
+          }
+        },
+      },
+      {
+        label: 'events',
+        onClick: () => {
+          if (menuIsExpanded || addGridExpanded){
+            closeMenu();
+            closeSSHContent();
+          }else{
+            setRouterOptions();
+          }
+        }
+      },
+      {
+        label: 'ssh',
+        onClick: () => {
+          if (addGridExpanded || menuIsExpanded){
+            closeMenu();
+            closeSSHContent();
+          }else{
+            setSSHContent();
+          }
+        },
+      },
+      'disconnect']} />
 );
 
 const mapStateToProps = (state) => ({
+  menuIsExpanded: state.getIn(['reducer', 'menuExpanded']),
+  addGridExpanded: state.getIn(['reducer', 'gridIsOpen']),
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch, props) => ({
   setSSHContent: () => {
-    //dispatch(setContent(<SSH />))
-    //dispatch(expandMenu(true));
-    dispatch(setGridIsOpen());
-  }
+    dispatch(setGridIsOpen(true));
+  },
+  closeSSHContent: () => {
+    dispatch(setGridIsOpen(false));
+  },
+  setRouterOptions: () => {
+    dispatch(setContent(<RouterOptions />));
+    dispatch(expandMenu(true));
+  },
+  setEventLog: () => {
+    dispatch(setContent(<EventLog />));
+    dispatch(expandMenu(true));
+  },
+  closeMenu: () => {
+    dispatch(expandMenu(false));
+  },
 });
 
 export const container = connect(mapStateToProps, mapDispatchToProps)(MenuContainer);
---
+++ src/router_options/RouterOptions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RouterOptions = () => (
+  <div>
+    Router Options
+  </div>
+);
+
+export default RouterOptions;
