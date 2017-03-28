833d075b37a21721586a4ece0e30a37946fd83d2
= <bradmedeiros0@gmail.com>
2017-03-27 22:10:45 -0700

    linting

--- src/components/addgrid/AddGrid.js
+++ src/components/addgrid/AddGrid.js
@@ -2,17 +2,15 @@ import { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import './style.css';
 
-const AddGrid = () => (
-  return <div className="components_add_grid" />;
-)
+const AddGrid = () => <div className="components_add_grid" />;
 
 AddGrid.propTypes = {
-    isExpanded:  PropTypes.bool,
+  isExpanded: PropTypes.bool,
 };
 
 const mapDispatchToProps = dispatch => ({
-    onGridItemClick: () =>  dispatch(expandMenu(false)),
-    lockGrid: lockState => dispatch(lock(lockState)),
+  onGridItemClick: () => dispatch(expandMenu(false)),
+  lockGrid: lockState => dispatch(lock(lockState)),
 });
 
 export const container = connect(undefined, mapDispatchToProps)(AddGrid);
--- src/ssh/SSH.js
+++ src/ssh/SSH.js
@@ -3,6 +3,6 @@ import React from 'react';
 
 const SSH_URL = 'http://localhost:4050';
 
-const SSH = ({}) => <iframe src={SSH_URL} style={{ width: '100%', height: '100%' }} />;
+const SSH = () => <iframe src={SSH_URL} style={{ width: '100%', height: '100%' }} />;
 
 export default SSH;
