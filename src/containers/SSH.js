41d6f252d7cc0906aa14d11c195673c2c4818542
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-05-29 03:19:31 -0700

    change ssh port

--- src/containers/SSH.js
+++ src/containers/SSH.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const SSH_URL = 'http://localhost:4050';
+const SSH_URL = 'http://localhost:9001';
 
 const SSH = () => <iframe src={SSH_URL} style={{ width: '100%', height: '100%' }} />;
 
