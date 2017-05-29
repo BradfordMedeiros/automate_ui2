8c899330c72346237f884cdfb9c0ee81d0994122
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-05-29 03:23:28 -0700

    ssh overlay frame style

--- src/containers/SSH.js
+++ src/containers/SSH.js
@@ -3,6 +3,11 @@ import React from 'react';
 
 const SSH_URL = 'http://localhost:9001';
 
-const SSH = () => <iframe src={SSH_URL} style={{ width: '100%', height: '100%' }} />;
+const SSH = () => (
+  <iframe
+    src={SSH_URL}
+    style={{ border: '0 none', height: '100%', width: '100%' }}
+  />
+);
 
 export default SSH;
