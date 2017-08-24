384f1b65817d1154ddd12b5276825ee96b3a02d7
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-08-23 22:44:38 -0700

    made event log use generic overlay ui overlay paradigm

--- src/containers/EventLog.js
+++ src/containers/EventLog.js
@@ -1,73 +1,9 @@
 import React from 'react';
-import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
 import WithData from '../data/WithData';
-
+import EventLogComponent from '../components/EventLog';
 const WithEvents = WithData.polling.WithEvents;
 
 class EventLog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fixedHeader: true,
-      fixedFooter: true,
-      stripedRows: true,
-      showRowHover: false,
-      selectable: false,
-      multiSelectable: false,
-      enableSelectAll: false,
-      deselectOnClickaway: true,
-      showCheckboxes: false,
-      height: '100%',
-    };
-  }
-  getContainer = content => (
-    <Table
-      height={this.state.height}
-      fixedHeader={this.state.fixedHeader}
-      fixedFooter={this.state.fixedFooter}
-      selectable={this.state.selectable}
-      multiSelectable={this.state.multiSelectable}
-    >
-      <TableHeader
-        displaySelectAll={this.state.showCheckboxes}
-        adjustForCheckbox={this.state.showCheckboxes}
-        enableSelectAll={this.state.enableSelectAll}
-      >
-        <TableRow>
-          <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{ textAlign: 'center' }}>
-            Events
-          </TableHeaderColumn>
-        </TableRow>
-        <TableRow>
-          <TableHeaderColumn tooltip="The ID">Event Name</TableHeaderColumn>
-          <TableHeaderColumn tooltip="The Status">Message</TableHeaderColumn>
-          <TableHeaderColumn tooltip="The Name">Timestamp</TableHeaderColumn>
-        </TableRow>
-      </TableHeader>
-      <TableBody
-        displayRowCheckbox={this.state.showCheckboxes}
-        deselectOnClickaway={this.state.deselectOnClickaway}
-        showRowHover={this.state.showRowHover}
-        stripedRows={this.state.stripedRows}
-      >
-        {content && content.data.map((item, index) => (
-          <TableRow>
-            <TableRowColumn>{index}</TableRowColumn>
-            <TableRowColumn>{item.topic}</TableRowColumn>
-            <TableRowColumn>{item.timestamp}</TableRowColumn>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  )
-  handleToggle = (event, toggled) => {
-    this.setState({
-      [event.target.name]: toggled,
-    });
-  };
-  handleChange = (event) => {
-    this.setState({ height: event.target.value });
-  };
   render() {
     return (
       <div style={{ background: 'rgb(40,40,40)' }}>
@@ -75,7 +11,7 @@ class EventLog extends React.Component {
           whileLoading={this.getContainer}
           refresh={1000}
         >
-          {content => this.getContainer(content)}
+          {content =>  <EventLogComponent data={content.data}/>}
         </WithEvents>
       </div>
     );
