5dad1d6616700a5b6b9d56f97eb4dbd7820ef761
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-09-15 01:15:41 -0700

    fix add tile to grid overlay text on smaller screens

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,11 +1,43 @@
 import React, { Component, PropTypes } from 'react';
 import { Paper } from 'material-ui';
 import { WidthProvider, Responsive } from 'react-grid-layout';
+import { Desktop, Mobile} from '../../util/ViewportSizing';
 import './react-grid-layout.css';
 import './react-resizable.css';
-//import '../../../node_modules/react-grid-layout/css/styles.css';
-//import '../../../node_modules/react-resizable/css/styles.css';
 import './style.css';
+
+const styles = {
+  addTileTextMobile: {
+    position: 'absolute',
+    left: '10%',
+    right: '10%',
+    display: 'flex',
+    justifyContent: 'center',
+    top: '30%',
+    color: 'rgb(210,210,210)',
+    borderTop: '1px solid grey',
+    borderBottom: '1px solid grey',
+    textShadow: '0px 1px 1px black',
+    fontFamily: 'monospace',
+    fontSize: 20,
+    padding: 14,
+  },
+  addTileTextDesktop: {
+    position: 'absolute',
+    left: '30%',
+    right: '30%',
+    display: 'flex',
+    justifyContent: 'center',
+    top: '30%',
+    color: 'rgb(210,210,210)',
+    borderTop: '1px solid grey',
+    borderBottom: '1px solid grey',
+    textShadow: '0px 1px 1px black',
+    fontFamily: 'monospace',
+    fontSize: 48,
+    padding: 14,
+  }
+};
 
 const ReactGridLayout = WidthProvider(Responsive);
 
@@ -30,27 +62,26 @@ class Grid extends Component {
           {(tileKeys.length === 0) && (
             <div style={{width: '100%', height: '100%', background: 'rgba(0,0,0,0.9)'}} />
             )}
-          {(tileKeys.length === 0) && (
-            <div
-              style={{
-                position: 'absolute',
-                left: '30%',
-                right: '30%',
-                display: 'flex',
-                justifyContent: 'center',
-                top: '30%',
-                color: 'rgb(210,210,210)',
-                borderTop: '1px solid grey',
-                borderBottom: '1px solid grey',
-                textShadow: '0px 1px 1px black',
-                fontFamily: 'monospace',
-                fontSize: 48,
-                padding: 14,
-              }}
-            >
-              Add tiles to the grid
-            </div>
-          )}
+
+          <Desktop>
+            {(tileKeys.length === 0) && (
+              <div
+                style={styles.addTileTextDesktop}
+              >
+                Add tiles to the grid
+              </div>
+            )}
+          </Desktop>
+          <Mobile>
+            {(tileKeys.length === 0) && (
+              <div
+                style={styles.addTileTextMobile}
+              >
+                Add tiles to the grid
+              </div>
+            )}
+          </Mobile>
+
           <ReactGridLayout
             layouts={{ xxs: jsLayout, xs: jsLayout, sm: jsLayout, md: jsLayout, lg: jsLayout }}
             breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
