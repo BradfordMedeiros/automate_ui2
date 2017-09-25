decd61f8dd92a807cb1598f8b4188006eef2407d
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-09-24 22:16:06 -0700

    added url hash override for automate url--  this might not be the best way, but is localized and should work for now

--- src/data/WithData.js
+++ src/data/WithData.js
@@ -1,6 +1,12 @@
 
 import generateWithData from './api/generateWithData';
 
+const isUrl = url => {
+  if (!url || url.length === 0){
+    return false;
+  }
+  return true;
+};
 const getDataConfig = (isProduction) => {
   if (isProduction == true && window.location.protocol !== 'file:') { //eslint-disable-line
     return ({
@@ -8,9 +14,12 @@ const getDataConfig = (isProduction) => {
       mqttBroker: `${window.location.protocol}//${window.location.hostname}:4000`,
     });
   }
+
+  const backendUrl  = isUrl(window.location.hash.substring(1)) ? window.location.hash.substring(1): '127.0.0.1' ;
+
   return ({
-    automateUrl: 'http://127.0.0.1:9000',
-    mqttBroker: 'http://127.0.0.1:4000',
+    automateUrl: `http://${backendUrl}:9000`,
+    mqttBroker: `http://${backendUrl}:4000`,
   });
 };
 
