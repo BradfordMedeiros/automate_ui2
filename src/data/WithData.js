418e2a33c34834502fa44c41ceabb0759f5b3d1a
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-09-24 23:25:36 -0700

    add url query parameter instead of hash

--- src/data/WithData.js
+++ src/data/WithData.js
@@ -1,12 +1,12 @@
 
 import generateWithData from './api/generateWithData';
 
-const isUrl = url => {
-  if (!url || url.length === 0){
-    return false;
-  }
-  return true;
+const getUrl = () => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const backendUrl = searchParams.get('url') ? searchParams.get('url') : '127.0.0.1';
+  return backendUrl;
 };
+
 const getDataConfig = (isProduction) => {
   if (isProduction == true && window.location.protocol !== 'file:') { //eslint-disable-line
     return ({
@@ -15,7 +15,7 @@ const getDataConfig = (isProduction) => {
     });
   }
 
-  const backendUrl  = isUrl(window.location.hash.substring(1)) ? window.location.hash.substring(1): '127.0.0.1' ;
+  const backendUrl = getUrl();
 
   return ({
     automateUrl: `http://${backendUrl}:9000`,
