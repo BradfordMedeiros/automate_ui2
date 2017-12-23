5e102e02b1bbd1cfbaea4118237a3fb74eccb8c6
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-12-22 23:00:53 -0800

    add upload

--- src/data/api/requests/getStyleSheet.js
+++ src/data/api/requests/getStyleSheet.js
@@ -4,14 +4,14 @@ import download from 'downloadjs';
 const getStyleSheetOperations = (AUTOMATE_CORE_URL) => {
   const themeUrl = `${AUTOMATE_CORE_URL}/theme`;
 
-  const deleteStylesheet = async () => {
+  const deleteStylesheet = async() => {
     const response = await fetch(themeUrl, {
       method: 'DELETE',
       mode: 'cors',
     });
   };
 
-  const downloadStylesheet = async () => {
+  const downloadStylesheet = async() => {
     const response = await fetch(themeUrl, {
       method: 'GET',
       mode: 'cors',
@@ -20,8 +20,19 @@ const getStyleSheetOperations = (AUTOMATE_CORE_URL) => {
     download(blob, "user_style.css", "text/plain");
   };
 
-  const uploadStylesheet =  stylesheetData => {
-    console.log('upload placeholder');
+  const uploadStylesheet = async stylesheetData => {
+    window.sd = stylesheetData;
+    const response = await fetch(themeUrl, {
+      method: 'POST',
+      mode: 'cors',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      }),
+      body: JSON.stringify({
+        style: stylesheetData,
+      }),
+    });
   };
 
   return ({
