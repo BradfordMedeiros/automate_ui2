f1d7c14cf6459daaceb37f9d59efa55d821cfd9f
brad <bradmedeiros0@gmail.com>
2018-05-17 21:02:51 -0700

    added css style loader

--- rebuild/webpack.config.js
+++ rebuild/webpack.config.js
@@ -1,28 +1,32 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
+      rules: [
           {
-            loader: "html-loader"
+              test: /\.css$/,
+              use: [ 'style-loader', 'css-loader' ]
+          },
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: "babel-loader"
+              }
+          },
+          {
+              test: /\.html$/,
+              use: [
+                  {
+                      loader: "html-loader"
+                  }
+              ]
           }
-        ]
-      }
-    ]
+      ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+        template: "./src/index.html",
+        filename: "./index.html"
     })
   ]
 };
