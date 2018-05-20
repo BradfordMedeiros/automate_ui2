73b8c737cbf488854b2d6f1ce253eae26588e3ef
brad <bradmedeiros0@gmail.com>
2018-05-20 00:17:20 -0700

    account management ui starting port

---
+++ rebuild/src/components/overlayContent/accountManagement/components/AdminSettings.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const styles = {
+ toggle: {
+   padding: 48,
+   boxShadow: '0px 0px 8px 1px black',
+ },
+ outer: {
+   marginTop: 80,
+ }
+};
+const AdminSettings = ({
+  allowUserCreation,
+  enableUserAccountCreation,
+  disableUserAccountCreation,
+}) => {
+  return (
+    <div style={styles.outer}>
+        <FormControlLabel
+            control={(
+                <Switch
+                //toggled={allowUserCreation}
+                onToggle={(_, toggleState) => {
+                    if (toggleState){
+                        enableUserAccountCreation();
+                    }else{
+                        disableUserAccountCreation();
+                    }
+                }}
+                //style={styles.toggle}
+                />)}
+            label="Allow Account Creation"
+        />
+
+
+
+
+
+      {/*
+       I want these features so leaving them in here even though they are not implemented
+       <Toggle style={styles.toggle} label="Allow Password Email Reset" />
+       <Toggle style={styles.toggle} label="Grid is Per User" />
+       <Toggle style={styles.toggle} label="Enable Guest User" />
+       */}
+    </div>
+  );
+}
+
+AdminSettings.propTypes = {
+  allowUserCreation: PropTypes.bool,
+  enableUserAccountCreation: PropTypes.func,
+  disableUserAccountCreation: PropTypes.func,
+};
+
+export default AdminSettings;
