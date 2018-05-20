644d5d2220536d47f0e648675f7512d34feed744
brad <bradmedeiros0@gmail.com>
2018-05-19 23:03:53 -0700

    overlay

---
+++ rebuild/src/components/overlay/Overlay.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+const style = {
+    expanded: {
+        animation: 'fade_in 0.1s  ease forwards',
+    },
+    not_expanded: {
+        animation: 'fade_out 0.1s  ease forwards',
+    }
+};
+
+class SelectionOverlay extends Component {
+    render() {
+        const { isExpanded, children } = this.props;
+        const desktopStyle = isExpanded ? style.expanded : style.not_expanded;
+
+        return (
+            <div className="overlay_outer">
+                <div className="overlay" style={{ ...desktopStyle }}>
+                    {children}
+                </div>
+            </div>
+        );
+    }
+};
+
+SelectionOverlay.propTypes = {
+    isExpanded: PropTypes.bool.isRequired,
+    children: PropTypes.node,
+};
+
+export default SelectionOverlay;
+
--- rebuild/src/index.js
+++ rebuild/src/index.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Appbar from './components/appbar/Appbar';
+import Overlay from './components/overlay/Overlay';
 import getData from './data/getData';
 import './style.css';
+import PropTypes from "prop-types";
 
 //const Data = getData();
 
-const MockApp = () => (
-    <div style={{
-        display: 'flex',
-        flexGrow: 1,
-        background: 'radial-gradient(rgb(30,30,30),rgb(20,20,20))'
-    }}>
-        <Appbar
-            title="automate"
-            showHideMenu
-        />
-    </div>
-);
+class MockApp extends Component {
+    state = {
+        isRotated: false,
+    };
+    render() {
+        return (
+            <div style={{
+                display: 'flex',
+                flexGrow: 1,
+                flexDirection: 'column',
+                background: 'radial-gradient(rgb(30,30,30),rgb(20,20,20))'
+            }}>
+                <Appbar
+                    title="automate"
+                    showHideMenu
+                    rotateAddIcon={this.state.isRotated}
+                    systemLocked={false}
+                    onRotatedAddIconClick={() => {
+                        console.log('rotated click');
+                        this.setState({
+                            isRotated: false,
+                        })
+                    }}
+                    onAddIconClick={() => {
+                        console.log('icon clicked');
+                        this.setState({
+                            isRotated: true,
+                        })
+                    }}
+                    onUserIconClick={() => {
+                        console.log('user icon clicked');
+                    }}
+                    onHideMenu={() => {
+                        console.log('hide menu clicked');
+                    }}
+                    onToggle={() => {
+                        console.log('toggle');
+                    }}
+                />
+
+                <Overlay isExpanded={this.state.isRotated}>
+                    <div>hello</div>
+                </Overlay>
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render(<MockApp />, document.getElementById("root"));
