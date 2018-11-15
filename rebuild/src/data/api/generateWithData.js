e265c301991b40ac4567521add79ccc072708819
brad medeiros <bradmedeiros0@gmail.com>
2018-11-15 00:12:57 -0800

    remove mongo

--- rebuild/src/data/api/generateWithData.js
+++ rebuild/src/data/api/generateWithData.js
@@ -5,7 +5,6 @@ import getWithStates from './polling/getWithStates';
 import getWithActions from './polling/getWithActions';
 import getWithSequences from './polling/getWithSequences';
 import getWithSystemInfo from './polling/getWithSystemInfo';
-import getWithMongo from './polling/getWithMongo';
 import getWithConditions from './polling/getWithConditions';
 import getWithStatus from './polling/getWithStatus';
 import getWithSchedules from './polling/getWithSchedules';
@@ -31,7 +30,6 @@ const generateWithData = ({ automateUrl, mqttBroker }) => {
       WithEvents: getWithPollingEvents(automateUrl),
       WithSequences: getWithSequences(automateUrl),
       WithSystemInfo: getWithSystemInfo(automateUrl),
-      WithMongo: getWithMongo(automateUrl),
       WithConditions: getWithConditions(automateUrl),
       WithSchedules: getWithSchedules(automateUrl),
       WithRules: getWithRules(automateUrl),
