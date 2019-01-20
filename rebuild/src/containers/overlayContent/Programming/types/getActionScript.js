d652222e86fb3edefce79b0d2577bdc459a5b597
brad medeiros <bradmedeiros0@gmail.com>
2019-01-19 23:54:31 -0800

    remove unused imports

--- rebuild/src/containers/overlayContent/Programming/types/getActionScript.js
+++ rebuild/src/containers/overlayContent/Programming/types/getActionScript.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import Header from '../../../../components/overlayContent/programming/components/Header/Header';
-import SelectableTypes from '../../../../components/overlayContent/programming/components/SelectableTypes/SelectableTypes';
-import AddItemDialog from '../../../../components/overlayContent/programming/components/AddItemDialog/AddItemDialog';
-import CodeEditor from '../../../../components/overlayContent/programming/components/CodeEditor'
-import EditorControls from '../../../../components/overlayContent/programming/components/EditorControls/EditorControls';
 import { ActionScript } from '../../../../components/overlayContent/programming/components/types/Types';
 
 const getActionScript = (WithActionScripts) => (
